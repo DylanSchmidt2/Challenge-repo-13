43a2fd5a319adb31bb486c1bfd7be5add963169f
Dylan Schmidt (19) <109780961+DylanSchmidt2@users.noreply.github.com>
2023-02-13 19:14:57 -0500

    Added same put, post, and get routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
+
 
 // get all products
 router.get('/', async (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+
   try {
     const productData = await Product.findAll({
       include: [{model: Category}, {model : Tag}],
@@ -17,7 +16,7 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
+
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
